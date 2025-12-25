134b2786871b9a7770f299f5de3f5701456ea28e
王华江 <1312513223@qq.com>
2025-12-25 11:54:57 +0800

    docs: 添加VuePress基础路径配置
    添加base配置项以支持项目部署在子路径下

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/vuepress-starter/',
   lang: 'en-US',
 
   title: 'VuePress',
